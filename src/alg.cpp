9e7f0359402eb82fbe067b0a962ec261ca68132e
TolyaChepaykin <160387810+TolyaChepaykin@users.noreply.github.com>
2024-03-18 21:44:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,19 +1,17 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-int m = size / 2;
-for (int i = 0; i < size; ++i) {
+int f = 0, l = size, m = 0;
+while (f <= l) {
+	m = f + (l - f) / 2;
 	if (value == arr[m]) {
 		return arr[m];
 	}
 	else if (value > arr[m]) {
-		i = m;
+		f = m;
 	}
 	else if (value < arr[m]) {
-		size = m;
+		l = m;
 	}
-	else {
-		return 0;
-	}
-}
+	return 0;
 }
